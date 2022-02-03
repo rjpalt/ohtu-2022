5b3b59be71df4ab5091cf45014ba1a2edb813b43
Rasmus Paltschik <rjpalt@gmail.com>
2022-02-03 13:33:53 +0200

    Add exercise 12

--- viikko_2/verkkokauppa-1/src/pankki.py
+++ viikko_2/verkkokauppa-1/src/pankki.py
@@ -1,10 +1,10 @@
-from kirjanpito import Kirjanpito
+from kirjanpito import kirjanpito as default_kirjanpito
 
 
 class Pankki:
 
-    def __init__(self, kirjanpito: 'Kirjanpito'):
-        self._kirjanpito = kirjanpito
+    def __init__(self, kirjanpito=default_kirjanpito):
+            self._kirjanpito = kirjanpito
 
     def tilisiirto(self, nimi, viitenumero, tililta, tilille, summa):
         self._kirjanpito.lisaa_tapahtuma(
@@ -13,3 +13,5 @@ class Pankki:
 
         # täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
         return True
+
+pankki = Pankki()
