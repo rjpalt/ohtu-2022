1251b186ff1a3c88e6e1d62a72b6f44961e2fd31
Rasmus Paltschik <rjpalt@gmail.com>
2022-02-11 23:30:35 +0200

    Check adding two tuote results in single ostos with same name and count 2

--- viikko_4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko_4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -111,3 +111,16 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
 
         self.assertEqual(len(ostokset), 1)
+
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_ostos_samalla_nimella_ja_lukumaara_kaksi(self):
+        
+        maito = Tuote("Maito", 3)
+        
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostokset = self.kori.ostokset()
+
+        self.assertEqual(ostokset[0].tuotteen_nimi(), 'Maito')
+        self.assertEqual(ostokset[0].lukumaara(), 2)
