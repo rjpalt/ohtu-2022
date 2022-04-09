e3f8afa0cbc8bef1bf6ae4783aeb4e5d2c11a66a
Rasmus Paltschik <rjpalt@gmail.com>
2022-04-09 15:39:46 +0300

    Exercises 6.2 and 6.3

--- viikko_6/query-language/src/index.py
+++ viikko_6/query-language/src/index.py
@@ -1,16 +1,55 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, Not, HasAtLeast, HasFewerThan, PlaysIn, All, Or
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+    #First matches
+    """
     matcher = And(
         HasAtLeast(5, "goals"),
         HasAtLeast(5, "assists"),
         PlaysIn("PHI")
+    )
+    """
+
+    ## For Not
+    """
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+    )
+    """
+
+    ## For HasFewerThan
+    """
+    matcher = And(
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
+    )
+    """
+    ##For All
+    #matcher = All()
+
+    ## 1. Or-testi:
+    """
+    matcher = Or(
+        HasAtLeast(30, "goals"),
+        HasAtLeast(50, "assists")
+    )
+    """
+
+    ## 2. Or-testi
+    matcher = And(
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
     )
 
     for player in stats.matches(matcher):
--- viikko_6/query-language/src/matchers.py
+++ viikko_6/query-language/src/matchers.py
@@ -9,6 +9,43 @@ class And:
         
         return True
 
+class All:
+    def matches(self, player):
+        return True
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return False
+
+        return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+
+        return False
+
 class PlaysIn:
     def __init__(self, team):
         self._team = team
